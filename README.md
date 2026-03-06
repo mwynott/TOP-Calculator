1525533ec9e4eeedb7322cd6d127e484616cc29d
mwynott <mwynott1989@gmail.com>
2026-03-06 15:20:55 -0500

    Refactor html slightly and implement operator and display functions

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # TOP-Calculator
-The TOP Calculator project
+The TOP Calculator Project:
+
+This took me a while to do. I actually began to lose hope with how
+paralyzed I felt starting this project. However, bit by bit I 
+added pieces and moved pieces around, and refactored. Often, I would
+have to go back and reread something from MDN or a similar site, and
+even then sometimes I would need to see examples. But, with time and 
+effort this finally came together. Despite many hours and an entire
+lack of committing to git as TOP recommends, I have finally made
+a calculator that is basic in functionality, but at least throws an
+error and resets the display if the user attempts to divide by zero.
+
+The lesson here for my future self, since no one is likely to ever
+check out this readme is this: Take it one step at a time. You 
+cannot hold the entire project in your head. Complete what you know
+and what is easy first (HTML, CSS), and then work on JS. Once there,
+create a single function to do even a single piece of what you
+want. From there, expand outward logically. Do not be afraid to
+refactor! Even if this means refactoring the entire project top to
+bottom, just do it. The second time around on this calculator 
+project really made me realize where my previous mistakes in logic
+were and where I might improve the readability of my code. 
+
+This is a long road, and the foundations course has been difficult,
+but there is a part of me that is driven to learn and to improve
+my problem solving abilities. I look forward to the challenge.
--- js.js
+++ js.js
@@ -1,5 +1,116 @@
-let firstOperand = null;
-let operator = null;
-let secondOperand = null;
-let ResetDisplay = false;
+//Calculator memory//
+
+let currentOperand = "0";
+let previousOperand = "";
+let operator = null
+let shouldResetScreen = false;
+
+//DOM//
+
+const currentDisplay = document.getElementById("current-operand");
+const previousDisplay = document.getElementById("previous-operand");
+
+
+//Event Listeners//
+
+document.querySelectorAll(".number").forEach(btn => {
+    btn.addEventListener("click", () => 
+        appendNumber(btn.dataset.number));
+});
+
+document.querySelectorAll(".operator").forEach(btn => {
+    btn.addEventListener("click", () => chooseOperator(btn.dataset.operator));
+});
+
+document.getElementById('equals').addEventListener('click', calculate);
+document.getElementById('clear').addEventListener('click', clearAll);
+document.getElementById('delete').addEventListener('click', deleteLast);
+
+
+
+//Functions//
+
+function updateDisplay() {
+    currentDisplay.textContent = currentOperand;
+    previousDisplay.textContent = previousOperand + 
+    (operator ? ` ${operator}` : ``);
+}
+
+//Start new number if user input operator//
+function appendNumber(number) {
+    if (shouldResetScreen) {
+        currentOperand = "";
+        shouldResetScreen = false;
+    }
+//Prevent multiple decimal points//
+    if (number === "." && currentOperand.includes(".")) return;
+//No leading 0//
+if (currentOperand === "0" && number !== ".") {
+    currentOperand = number;
+} else {
+    currentOperand += number;
+}
+updateDisplay();
+}
+
+function calculate() {
+    if (!operator || previousOperand === "") return;
+
+    const prev = parseFloat(previousOperand);
+    const current = parseFloat(currentOperand);
+    let result;
+
+    switch(operator) {
+        case "+": result = prev + current; break;
+        case "-": result = prev - current; break;
+        case "*": result = prev * current; break;
+        case "÷": //Divide by 0 check//
+            if (current === 0) {
+                currentOperand = "Error";
+                operator = null;
+                previousOperand = "";
+                updateDisplay();
+                return;
+            }
+            result = previous / current;
+            break;
+    }
+    //Limits floating numbers to ten decimal places//
+    currentOperand = parseFloat(result.toFixed(10)).toString();
+    operator = null;
+    previousOperand = "";
+    shouldResetScreen = true;
+    updateDisplay();
+}
+
+function chooseOperator(op) {
+    //Calculate operation first to enable chaining operations//
+    if (previousOperand !== "" && !shouldResetScreen) {
+        calculate();
+    }
+
+operator = op;
+previousOperand = currentOperand;
+shouldResetScreen = true; //Clear screen with next number//
+updateDisplay();
+}
+
+function clearAll() {
+    currentOperand = "0";
+    previousOperand = "";
+    operator = null;
+    shouldResetScreen = false;
+    updateDisplay();
+}
+
+//Delete last number entered//
+function deleteLast() {
+    if (currentOperand.length === 1 || currentOperand === "Error") {
+        currentOperand = "0";
+    } else {
+        currentOperand = currentOperand.slice(0, -1);
+    }
+    updateDisplay();
+}
+
 
